f2c82e0e936625d4436449e002dd4f1255f0050e
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2024-05-16 19:34:04 +0300

    Update combine_evals.R

--- combine_evals.R
+++ combine_evals.R
@@ -1,3 +1,8 @@
+moc_evals     <- read_csv("moc_evals.csv")
+nice_pr_evals <- read_csv("nice_pr_evals.csv")
+nice_sp_evals <- read_csv("nice_sp_evals.csv")
+wi_evals      <- read_csv("wi_evals.csv")
+
 evals <- rbind(data.frame(method = rep("MOC",     dim(moc_evals)[1]),     moc_evals),
                data.frame(method = rep("NiCE_pr", dim(nice_pr_evals)[1]), nice_pr_evals),
                data.frame(method = rep("NiCE_sp", dim(nice_sp_evals)[1]), nice_sp_evals),
